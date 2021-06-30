5491432ac989adfaa333f6ba013c6cbacfd668de
zuhair12390 <78954298+zuhair12390@users.noreply.github.com>
2021-07-01 00:47:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+
+const links = document.querySelector('.links');
+const bars = document.querySelector('.bars');
+const inputShorten = document.querySelector('input[placeholder="Shorten a link here..."]');
+const shorten = document.querySelector('#shorten');
+const newshort = document.querySelector('.short');
+let test = true;
+bars.addEventListener('click', function () {
+    if (test) {
+        links.style = 'left: 0; transition: 1s;';
+        this.style = 'transform: rotateZ(90deg); transition: 1s';
+        test = false
+    } else {
+        links.style = 'left: -200%; transition: 1s;';
+        this.style = 'transform: rotateZ(0deg); transition: 1s';
+        test = true;
+    }
+})
+let link = [];
+shorten.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (inputShorten.value.startsWith('http://') || inputShorten.value.startsWith('https://')) {
+        link.push(inputShorten.value);
+        localStorage.setItem('link', JSON.stringify(link));
+        newshort.innerHTML +=
+        `<div class="newlink">
+            <p class="linkCopy"><a href="${inputShorten.value}">${inputShorten.value}</a></p>
+            <button class"copy">Copy</button>
+        </div>`
+        inputShorten.value = '';
+        inputShorten.placeholder= "Shorten a link here...";
+        inputShorten.style.border = 'none';
+        abc();
+    }else {
+        inputShorten.value = '';
+        inputShorten.placeholder = 'Please enter a valid URL';
+        inputShorten.style.border = '1px solid red';
+    }
+});
+
+for (let item of JSON.parse(localStorage.getItem('link')) || link) {
+    newshort.innerHTML +=
+        `<div class="newlink">
+            <p class="linkCopy"><a href="${item}">${item}</a></p>
+            <button class"copy">Copy</button>
+        </div>`
+}
+
+
+function abc() {
+    document.querySelectorAll('.newlink').forEach(a => {
+        a.addEventListener('click', function abc(e) {
+            if (e.target.classList = 'copy') {
+                let copytext = this.firstElementChild.textContent;
+                function copys(text) {
+                    let inputs = document.createElement('input');
+                    inputs.setAttribute('value', text);
+                    document.body.appendChild(inputs);
+                    inputs.select();
+                    let result = document.execCommand('copy');
+                    document.body.removeChild(inputs);
+                    return result;
+                }
+                copys(copytext)
+            }
+        })
+    })
+}
+abc();
+
+fetch('https://api.shrtco.de/v2/shorten?').then(res => res.json())
+.then(data => {
+    console.log(data);
+})
