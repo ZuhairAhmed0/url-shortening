58ef28a35d68d5351b163b30cdf09268ac78e0fd
zuhair12390 <78954298+zuhair12390@users.noreply.github.com>
2021-07-01 12:57:26 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-
 const links = document.querySelector('.links');
 const bars = document.querySelector('.bars');
 const inputShorten = document.querySelector('input[placeholder="Shorten a link here..."]');
 const shorten = document.querySelector('#shorten');
 const newshort = document.querySelector('.short');
 let test = true;
-bars.addEventListener('click', function () {
+bars.addEventListener('click', function() {
     if (test) {
         links.style = 'left: 0; transition: 1s;';
         this.style = 'transform: rotateZ(90deg); transition: 1s';
@@ -17,21 +16,30 @@ bars.addEventListener('click', function () {
     }
 })
 let link = [];
-shorten.addEventListener('click', function (e) {
+shorten.addEventListener('click', function(e) {
     e.preventDefault();
     if (inputShorten.value.startsWith('http://') || inputShorten.value.startsWith('https://')) {
-        link.push(inputShorten.value);
-        localStorage.setItem('link', JSON.stringify(link));
-        newshort.innerHTML +=
-        `<div class="newlink">
-            <p class="linkCopy"><a href="${inputShorten.value}">${inputShorten.value}</a></p>
-            <button class"copy">Copy</button>
-        </div>`
+        let url = inputShorten.value.slice(0, 7) == 'http://' ? inputShorten.value.slice(7) : inputShorten.value.slice(8);
+        fetch(`https://api.shrtco.de/v2/shorten?url=${url}`, { method: 'POST' })
+            .then(res => res.json())
+            .then(data => {
+                link.push(data.result.full_short_link);
+                localStorage.setItem('link', JSON.stringify(link));
+                newshort.innerHTML +=
+                    `<div class="newlink">
+                            <p class="linkCopy"><a href="${data.result.full_short_link}">${data.result.full_short_link}</a></p>
+                            <button class"copy">Copy</button>
+                        </div>`
+
+                abc();
+            })
+            .catch((error) => {
+                console.log(error);
+            })
         inputShorten.value = '';
-        inputShorten.placeholder= "Shorten a link here...";
+        inputShorten.placeholder = "Shorten a link here...";
         inputShorten.style.border = 'none';
-        abc();
-    }else {
+    } else {
         inputShorten.value = '';
         inputShorten.placeholder = 'Please enter a valid URL';
         inputShorten.style.border = '1px solid red';
@@ -52,6 +60,7 @@ function abc() {
         a.addEventListener('click', function abc(e) {
             if (e.target.classList = 'copy') {
                 let copytext = this.firstElementChild.textContent;
+
                 function copys(text) {
                     let inputs = document.createElement('input');
                     inputs.setAttribute('value', text);
@@ -67,8 +76,3 @@ function abc() {
     })
 }
 abc();
-
-fetch('https://api.shrtco.de/v2/shorten?').then(res => res.json())
-.then(data => {
-    console.log(data);
-})
